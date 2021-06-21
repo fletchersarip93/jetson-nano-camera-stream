ed3381cb8b9488d32e31e0121fd835b27cad22a6
Fletcher Sarip <fletchersarip@gmail.com>
2021-06-21 21:14:55 +0800

    bugfix: Fix the REST API response to return encoded image.

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 import threading
 from flask import Response, Flask
 import cv2
+import time
 
 app = Flask(__name__)
 
 global video_frame
+video_frame = None
 
 global thread_lock
 thread_lock = threading.Lock()
@@ -16,7 +18,8 @@ fps = 21
 capture_width = 3280
 capture_height = 2464
 
-GSTREAMER_PIPELINE = ('nvarguscamerasrc ! video/x-raw(memory:NVMM), '
+
+GSTREAMER_PIPELINE = ('nvarguscamerasrc ! ideo/x-raw(memory:NVMM), '
     f'width={capture_width}, height={capture_height}, '
     f'format=(string)NV12, framerate=(fraction){fps}/1 ! nvvidconv ! '
     f'video/x-raw, width=(int){width}, height=(int){height}, format=(string)BGRx ! videoconvert ! '
@@ -30,22 +33,25 @@ def get_video_frame():
 
     # use the cv2 VideoCapture
     video_capture = cv2.VideoCapture(GSTREAMER_PIPELINE, cv2.CAP_GSTREAMER)
+    print('video_capture.isOpened:', video_capture.isOpened())
 
     while video_capture.isOpened():
         return_key, frame = video_capture.read()
         if not return_key:
+            print('return_key break:', return_key)
             break
         
         # save the frame into the global variable
         with thread_lock:
             video_frame = frame.copy()
 
-        key = cv2.waitKey(30) & 0xff
+        key = cv2.waitKey(100) & 0xff
         if key == 27:
+            print('key break:', key)
             break
     
     video_capture.release()
-
+    print('video_capture released')
 
 # flask application behaviors
 def stream_encoded_frame():
@@ -54,31 +60,33 @@ def stream_encoded_frame():
     while True:
         with thread_lock:
             global video_frame
-
             if video_frame is None:
                 continue
-
             return_key, encoded_frame = cv2.imencode(".jpg", video_frame)
-
             if not return_key:
                 continue
 
         yield (b'--frame\r\n'
             b'Content-Type:image/jpeg\r\n\r\n'
-            + bytearray(video_frame) +
+            + bytearray(encoded_frame) +
             b'\r\n')
 
-@app.route('/')
+@app.route('/stream')
 def stream_video():
-    return Response(stream_encoded_frame(), mimetype='multipart/x-mixed-replace;boundary=frame')
+    return Response(stream_encoded_frame(), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+@app.route('/hello')
+def hello():
+    return 'hello'
 
 # main thread
 if __name__ == '__main__':
+
     # start the thread for getting video frames
     get_video_thread = threading.Thread(target=get_video_frame)
     get_video_thread.daemon = True
     get_video_thread.start()
 
-
     # start the flask application
     app.run('0.0.0.0', port='8000')
+
